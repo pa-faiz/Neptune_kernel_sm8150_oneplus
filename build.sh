1559a02d94d67a2d32d5838ce4f969eef4a39d41
Harish <sanganiharish263@gmail.com>
2022-06-11 19:58:29 +0530

    Fix script
    Signed-off-by: Harish <sanganiharish263@gmail.com>

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ export HOME=/home/harish/neptune
 export GCC_PATH=$HOME/gcc-arm64/bin
 export PATH="$GCC_PATH:$PATH"
 export CROSS_COMPILE=$HOME/gcc-arm64/bin/aarch64-elf-
+export CROSS_COMPILE_ARM32=$HOME/gcc-arm/bin/arm-eabi-
 export KBUILD_BUILD_USER=Codecity001
 export KBUILD_BUILD_HOST=Harish
 
