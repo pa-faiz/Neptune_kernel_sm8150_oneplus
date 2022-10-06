289d10371072d944ef8fe2bca2f291ca56b25c31
Harish <sanganiharish263@gmail.com>
2022-10-06 08:50:34 +0000

    neptune: Import build scripts
    Signed-off-by: Harish <sanganiharish263@gmail.com>

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# HOME path
+export HOME=/home/harish/neptune
+
+# Compiler environment
+export CLANG_PATH=$HOME/rui-clang/bin
+export PATH="$CLANG_PATH:$PATH"
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export KBUILD_BUILD_USER=Codecity001
+export KBUILD_BUILD_HOST=Harish
+
+echo
+echo "Setting defconfig"
+echo
+
+make CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip neptune_defconfig
+
+echo
+echo "Compiling kernel"
+echo
+
+make CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(nproc --all) || exit 1
