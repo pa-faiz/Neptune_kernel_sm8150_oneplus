23ef3b3c2f2edc923a4691a73cd818e9941d2ae8
John Galt <johngaltfirstrun@gmail.com>
2023-01-17 13:26:43 -0500

    qpnp-power-on: add back OEM additions
    Some users had random and rare full reboots after this.
    
    Signed-off-by: Harish <sanganiharish263@gmail.com>

--- include/linux/input/qpnp-power-on.h
+++ include/linux/input/qpnp-power-on.h
@@ -61,6 +61,9 @@ struct qpnp_pon {
 	struct pon_regulator	*pon_reg_cfg;
 	struct list_head	list;
 	struct delayed_work	bark_work;
+	struct delayed_work     press_work;
+	struct delayed_work press_pwr;
+	struct work_struct  up_work;
 	atomic_t	   press_count;
 	struct dentry		*debugfs;
 	struct device_node      *pbs_dev_node;
