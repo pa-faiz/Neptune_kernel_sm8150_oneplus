0251152faec6516327f2f38db619c387a415c4fe
Harish <sanganiharish263@gmail.com>
2023-01-24 16:21:22 +0000

    Revert "Revert "drm: revert to on press fod behavior""
    This reverts commit 6687c17764aed25caaa1efb6c28c8983047abb4f.

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -1114,22 +1114,7 @@ static ssize_t oneplus_display_set_dim_alpha(struct device *dev,
 	return count;
 }
 
-int oneplus_auth_status = 0;
 int oneplus_panel_status = 0;
-static ssize_t op_display_get_auth_status(struct device *dev,
-				struct device_attribute *attr, char *buf)
-{
-	return sprintf(buf, "%d\n", oneplus_auth_status);
-}
-
-static ssize_t op_display_set_auth_status(struct device *dev,
-				struct device_attribute *attr,
-				const char *buf, size_t count)
-{
-	sscanf(buf, "%d", &oneplus_auth_status);
-
-	return count;
-}
 
 static ssize_t op_display_get_dimlayer_enable(struct device *dev,
 				struct device_attribute *attr, char *buf)
@@ -1229,7 +1214,6 @@ static DEVICE_ATTR(dimlayer_bl_en, S_IRUGO|S_IWUSR, op_display_get_dimlayer_enab
 static DEVICE_ATTR(dp_en, S_IRUGO|S_IWUSR, op_display_get_dp_enable, op_display_set_dp_enable);
 static DEVICE_ATTR(dither_en, S_IRUGO|S_IWUSR, op_display_get_dither_enable, op_display_set_dither_enable);
 
-static DEVICE_ATTR(auth_status, S_IRUGO|S_IWUSR, op_display_get_auth_status, op_display_set_auth_status);
 
 static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_status.attr,
@@ -1265,7 +1249,6 @@ static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_dimlayer_bl_en.attr,
 	&dev_attr_dp_en.attr,
 	&dev_attr_dither_en.attr,
-	&dev_attr_auth_status.attr,
 	NULL
 };
 
--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -5556,7 +5556,6 @@ bool aod_complete;
 bool real_aod_mode;
 extern bool oneplus_dimlayer_hbm_enable;
 bool backup_dimlayer_hbm = false;
-extern int oneplus_auth_status;
 extern int oneplus_dim_status;
 int backup_dim_status = 0;
 int dsi_panel_enable(struct dsi_panel *panel)
@@ -5608,19 +5607,6 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	}
 
 	panel->panel_initialized = true;
-
-	if (oneplus_auth_status == 2) {
-		backup_dimlayer_hbm = 0;
-		backup_dim_status = 0;
-	} else if (oneplus_auth_status == 1) {
-		backup_dimlayer_hbm = 1;
-		backup_dim_status = 1;
-	}
-	oneplus_dimlayer_hbm_enable = backup_dimlayer_hbm;
-	oneplus_dim_status = backup_dim_status;
-	if (oneplus_auth_status != 2)
-		pr_err("Restore dim when panel goes on");
-	oneplus_auth_status = 0;
 
 	blank = MSM_DRM_BLANK_UNBLANK_CHARGE;
 	notifier_data.data = &blank;
