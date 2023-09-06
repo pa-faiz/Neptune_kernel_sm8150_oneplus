d2ee62cd1df4fafcacc515affdd55e61af181694
Codecity001 <sanganiharish263@gmail.com>
2023-09-06 20:16:22 +0000

    Conditionally apply GPU OC
    Signed-off-by: Codecity001 <sanganiharish263@gmail.com>

--- pack.sh
+++ pack.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
-
 # Kernel version configuration
 KNAME="NeptunePlayground"
 MIN_HEAD=$(git rev-parse HEAD)
 VERSION="$(cat version)-$(date +%m.%d.%y-%H%M)"
+
 if [[ "${1}" == "k" ]] ; then
-	ZIPNAME="${KNAME}-$(cat version)-KSu-$(echo $(date +%d.%m.%Y-%H%M))"
+    if [[ "${2}" == "o" ]] ; then
+        ZIPNAME="${KNAME}-$(cat version)-KSuOC-$(date +%d.%m.%Y-%H%M)"
+    else
+        ZIPNAME="${KNAME}-$(cat version)-KSu-$(date +%d.%m.%Y-%H%M)"
+    fi
 else
-	ZIPNAME="${KNAME}-$(cat version)-$(echo $(date +%d.%m.%Y-%H%M))"
+    if [[ "${1}" == "o" || "${2}" == "o" ]] ; then
+        ZIPNAME="${KNAME}-$(cat version)-OC-$(date +%d.%m.%Y-%H%M)"
+    else
+	ZIPNAME="${KNAME}-$(cat version)-$(date +%d.%m.%Y-%H%M)"
+    fi
 fi
 
-export LOCALVERSION="-${KNAME}-$(echo "${VERSION}")"
-
+export LOCALVERSION="-${KNAME}-${VERSION}"
 # Never dirty compile
 ./clean.sh
 
 # Build with kernel SU
 if [[ "${1}" == "k" ]] ; then
+	echo
 	echo "Building with Kernel SU"
 	patch -p1 < kernelsu.patch
 	curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+	if [[ "${2}" == "o" ]] ; then
+		echo
+		echo "Applying GPUOC.patch"
+		patch -p1 < GPUOC.patch
+	fi
+else
+	if [[ "${1}" == "o" ]] ; then
+		echo
+		echo "Applying GPUOC.patch"
+		patch -p1 < GPUOC.patch
+	fi
 fi
 
 # Let's build
 START=$(date +"%s")
 ./build.sh || exit 1
-
 # Kernel Output
 if [ -e arch/arm64/boot/Image.gz ] ; then
 	echo
@@ -45,9 +63,7 @@ if [ -e arch/arm64/boot/Image.gz ] ; then
 	cd ..
 	ls -al $ZIPNAME.zip
 fi
-
 rm -rf KernelSU
-
 # Show compilation time
 END=$(date +"%s")
 DIFF=$((END - START))
